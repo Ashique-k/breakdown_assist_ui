c95655a5d7d01f63fa1ad6f36f8c4a5d764689e8
Ashique-k <ashikmessi3@gmail.com>
2024-03-15 12:27:59 +0530

    1st

---
+++ lib/admin/admin_login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ADMIN_LOGIN extends StatefulWidget {
+  const ADMIN_LOGIN({super.key});
+
+  @override
+  State<ADMIN_LOGIN> createState() => _ADMIN_LOGINState();
+}
+
+class _ADMIN_LOGINState extends State<ADMIN_LOGIN> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
